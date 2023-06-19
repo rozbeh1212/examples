91f7e22b6b18d77e094ecd6735f245b572de8d92
rozbeh1212 <35690647+rozbeh1212@users.noreply.github.com>
2023-06-19 08:50:00 +0330

    Update index.py
    1

--- python/flask/api/index.py
+++ python/flask/api/index.py
@@ -1,11 +1,9 @@
-from flask import Flask
 
+from flask import Flask, jsonify
+import gdown
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/run-colab')
+def run_colab():
+    gdown.download('https://colab.research.google.com/drive/1BFm5YHSId6LjaM4lUi94Pnw-QNTnw5ar?usp=drive_link', 'colab.ipynb', quiet=False)
+    return jsonify(message='colab notebook ran successfully')
